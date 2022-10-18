8c6ab1fcc635dd50aebb4e39415d3c0f0000b1ab
lucuma <sbosell@gmail.com>
2022-10-17 22:21:39 -0500

    Create code.cs

---
+++ code.cs
@@ -0,0 +1,40 @@
+public async Task Any(AutoLikeSchedule req)
+        {
+
+            var authDetails = AuthRepository.GetUserAuthDetails(req.Id);
+            var provider = authDetails.FirstOrDefault(x => x.Provider == "twitter");
+            if (provider == null) return;
+
+            var userCredentials = new TwitterCredentials(
+                AppSettings.GetString("oauth.twitter.ConsumerKey"),
+                AppSettings.GetString("oauth.twitter.ConsumerSecret"),
+                provider.AccessToken, provider.AccessTokenSecret);
+
+            var userClient = new TwitterClient(userCredentials);
+            var data = userClient.Timelines.GetMentionsTimelineIterator();
+            int count = 0;
+            while (!data.Completed && count < 1)
+            {
+                var page = await data.NextPageAsync();
+                foreach (var tweet in page)
+                {
+                    if (!tweet.Favorited)
+                    {
+                        try
+                        {
+                            await tweet.FavoriteAsync();
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+
+                    }
+
+                    var t= await userClient.TweetsV2.GetTweetAsync("");
+                  
+                }
+                count++;
+            }
+
+        }
